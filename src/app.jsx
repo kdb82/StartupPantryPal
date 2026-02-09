f2bfe8f3957435e7579cb92f855fe235570da4d4
Kaden Bradshaw <kdnbradshaw@gmail.com>
2026-02-09 15:43:14 -0700

    first render

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import './styles.css';
 import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
-import { Login } from ".login/login";
-import { Register } from "./register/register";
-import { Pantry } from "./pantry/pantry";
-import { Recipes } from "./recipes/recipes";
-import { Calendar } from "./calendar/calendar";
-import { AILanding } from "./ailanding/ailanding";
+import { Login } from "./login/login";
+// import { Register } from "./register/register";
+// import { Pantry } from "./pantry/pantry";
+// import { Recipes } from "./recipes/recipes";
+// import { Calendar } from "./calendar/calendar";
+// import { AILanding } from "./ailanding/ailanding";
  
 export default function App() {
 	return (
 		<BrowserRouter>
-			<div class="app">
-				<header class="app-header">
-					<div class="container">
+			<div className="app">
+				<header className="app-header">
+					<div className="container">
 						<h1>PantryPal</h1>
-						<div class="navigation-container">
+						<div className="navigation-container">
 							<nav aria-label="Primary">
-								<ul class="nav-list">
+								<ul className="nav-list">
 									<li><a href="index.html" aria-current="page">Home</a></li>
 									<li><a href="pantry.html">Pantry</a></li>
 									<li><a href="recipes.html">Recipes</a></li>
@@ -33,10 +33,10 @@ export default function App() {
 					{/* Define your routes here */}
 				</Routes>
 
-				<footer class="app-footer">
-					<div class="container">
-						<p class="built-by">Built by Kaden Bradshaw</p>
-						<p class="github-link">
+				<footer className="app-footer">
+					<div className="container">
+						<p className="built-by">Built by Kaden Bradshaw</p>
+						<p className="github-link">
 							<a
 								href="https://github.com/kdb82/StartupPantryPal"
 								target="_blank"
