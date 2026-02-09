2fbdcc966fd85881835bbdd7ec329b71b87967ff
Kaden Bradshaw <kdnbradshaw@gmail.com>
2026-02-09 15:59:35 -0700

    recipes component

---
+++ src/aiLanding/ailanding.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function AILanding() {
+    return (
+        <main id="main" class="app-main">
+                <div class="container">
+                    <h1>AI Landing Page</h1>
+                </div>
+            </main>
+    );
+}
---
+++ src/calendar/calendar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Calendar() {
+    return (
+        <main id="main" class="app-main">
+                <div class="container">
+                    <h1>Calendar Page</h1>
+                </div>
+            </main>
+    );
+}
