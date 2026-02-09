7fbee432deaf6596da49c50cff1af511f7ef092d
Kaden Bradshaw <kdnbradshaw@gmail.com>
2026-02-09 15:31:13 -0700

    importing Routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import './styles.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
+import { Login } from ".login/login";
+import { Register } from "./register/register";
+import { Pantry } from "./pantry/pantry";
+import { Recipes } from "./recipes/recipes";
+import { Calendar } from "./calendar/calendar";
+import { AILanding } from "./ailanding/ailanding";
+ 
 export default function App() {
 	return (
 		<BrowserRouter>
