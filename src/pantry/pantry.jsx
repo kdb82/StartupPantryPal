7da4bc3e40243c56e7a05952bff917574d7eefbb
Kaden Bradshaw <kdnbradshaw@gmail.com>
2026-02-09 15:59:35 -0700

    recipes component

---
+++ src/pantry/pantry.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Pantry() {
+    return (
+        <main id="main" class="app-main">
+                <div class="container">
+                    <h1>Pantry Page</h1>
+                </div>
+            </main>
+    );
+}
---
+++ src/register/register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Register() {
+    return (
+        <main id="main" class="app-main">
+                <div class="container">
+                    <h1>AI Landing Page</h1>
+                </div>
+            </main>
+    );
+}
