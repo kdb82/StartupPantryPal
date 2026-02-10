50ab18be926edb1fcfa5af537d805b650c527734
Kaden Bradshaw <kdnbradshaw@gmail.com>
2026-02-09 18:10:32 -0700

    styles

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './styles.css';
+import "./styles.css";
 import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
 import { Login } from "./login/login";
 import { Register } from "./register/register";
